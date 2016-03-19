ad4fdc4d66036844fd240433f8953416851a7e49
Lin Huang <hl@rock-chips.com>
2016-03-19 22:45:19 +0800

    rockchip: rk3399: add grf define
    add grf define, we can do iomux and secure setting
    
    Change-Id: I4c228ddb60c9c4056de50312dc269227fac9a7fa
    Signed-off-by: huang lin <hl@rock-chips.com>

--- src/mainboard/google/gru/bootblock.c
+++ src/mainboard/google/gru/bootblock.c
@@ -14,12 +14,27 @@
  *
  */
 
+#include <arch/io.h>
+#include <assert.h>
 #include <bootblock_common.h>
+#include <soc/grf.h>
+#include <soc/spi.h>
+#include <console/console.h>
 
 void bootblock_mainboard_early_init(void)
 {
+	if (IS_ENABLED(CONFIG_DRIVERS_UART)) {
+		assert(CONFIG_CONSOLE_SERIAL_UART_ADDRESS == UART2_BASE);
+		write32(&rk3399_grf->iomux_uart2c, IOMUX_UART2C);
+
+		/* grf soc_con7[11:10] use for uart2 select*/
+		write32(&rk3399_grf->soc_con7, UART2C_SEL);
+	}
 }
 
 void bootblock_mainboard_init(void)
 {
+	write32(&rk3399_pmugrf->spi1_rxd, IOMUX_SPI1_RX);
+	write32(&rk3399_pmugrf->spi1_csclktx, IOMUX_SPI1_CSCLKTX);
+	rockchip_spi_init(CONFIG_BOOT_MEDIA_SPI_BUS, 24750*KHz);
 }
