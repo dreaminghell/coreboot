aed12e1a96aaab70847d295dd57452d7de843cb9
Lin Huang <hl@rock-chips.com>
2016-03-21 15:00:48 +0800

    TEST_ONLY: rockchip: support rk3399 evb board
    Change-Id: I96b708f36c9d2a31e3edf9acad262b5bbb4ecfce
    Signed-off-by: Lin Huang <hl@rock-chips.com>

---
+++ src/mainboard/google/rk3399evb/mainboard.c
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2016 Rockchip Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <arch/cache.h>
+#include <arch/io.h>
+#include <boardid.h>
+#include <boot/coreboot_tables.h>
+#include <device/device.h>
+#include <console/console.h>
+#include <gpio.h>
+#include <soc/clock.h>
+#include <soc/grf.h>
+
+static void configure_sdmmc(void)
+{
+	gpio_output(GPIO(0, A, 1), 1);	/* SDMMC_PWR_EN */
+	gpio_input(GPIO(0, A, 7));	/* SDMMC_DET_L */
+	write32(&rk3399_grf->iomux_sdmmc, IOMUX_SDMMC);
+}
+
+static void configure_emmc(void)
+{
+	/* emmc core clock multiplier */
+	rk_clrsetreg(&rk3399_grf->emmccore_con[11], 0xff << 0, 0 << 0);
+
+	rkclk_configure_emmc();
+}
+
+static void mainboard_init(device_t dev)
+{
+	configure_sdmmc();
+	configure_emmc();
+}
+
+static void mainboard_enable(device_t dev)
+{
+	dev->ops->init = &mainboard_init;
+}
+
+struct chip_operations mainboard_ops = {
+	.name = "rk3399evb",
+	.enable_dev = mainboard_enable,
+};
