0a14ef441318c46654dff8a098998f60f6a4bbee
Vadim Bendebury <vbendeb@chromium.org>
2016-03-24 00:16:04 -0700

    Build both evb and kevin from the same overlay
    Change-Id: Icb864de616b439dc8565b4a96512ed1ed0b03b7b
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- src/mainboard/google/kevin/mainboard.c
+++ src/mainboard/google/kevin/mainboard.c
@@ -18,11 +18,25 @@
 #include <arch/io.h>
 #include <boardid.h>
 #include <boot/coreboot_tables.h>
-#include <device/device.h>
 #include <console/console.h>
+#include <device/device.h>
+#include <gpio.h>
+#include <soc/clock.h>
+#include <soc/grf.h>
+
+static void configure_sdmmc(void)
+{
+#if CONFIG_EVB_MODE
+	gpio_output(GPIO(0, A, 1), 1);	/* SDMMC_PWR_EN */
+	gpio_input(GPIO(0, A, 7));	/* SDMMC_DET_L */
+	write32(&rk3399_grf->iomux_sdmmc, IOMUX_SDMMC);
+#endif
+}
 
 static void mainboard_init(device_t dev)
 {
+	configure_sdmmc();
+	rkclk_configure_emmc();
 }
 
 static void mainboard_enable(device_t dev)
@@ -31,6 +45,6 @@ static void mainboard_enable(device_t dev)
 }
 
 struct chip_operations mainboard_ops = {
-	.name = "kevin",
+	.name = CONFIG_MAINBOARD_PART_NUMBER,
 	.enable_dev = mainboard_enable,
 };
